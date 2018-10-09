90e8528f1003c018a088671b68b4d9dd194780e0
Wambui <wambui54mwangi@gmail.com>
2018-10-09 12:31:12 +0300

    new

---
+++ database/migrations/2018_10_09_084110_create_scouts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateScoutsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('scouts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('Name');
+            $table->string('Number');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('scouts');
+    }
+}
